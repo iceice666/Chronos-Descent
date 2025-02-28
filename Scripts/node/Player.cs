1539fcb50d44ec6d01880fe86930999b442845c4
iceice666 <iceice666@outlook.com>
2025-02-28 09:05:34 +0800

    flip sprite based on current velocity

--- Scripts/node/Player.cs
+++ Scripts/node/Player.cs
@@ -2,39 +2,22 @@ using Godot;
 
 namespace ChronosDescent.Scripts.node;
 
-public partial class Player : CharacterBody2D
+public partial class Player : Entity
 {
-    public int MoveSpeed = 300;
-    public int MaxMoveSpeed = 100;
-
-    // For `onready`
-    private AnimatedSprite2D _sprite;
-
-
-    public override void _Ready()
-    {
-        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-    }
-
-
-    public override void _Process(double delta)
-    {
-    }
-
     public override void _PhysicsProcess(double delta)
     {
         var direction = new Vector2(
             Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
             Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up")
         );
-
+        
         if (direction.Length() > 1.0)
         {
             direction = direction.Normalized();
         }
-
-        Velocity = direction * MoveSpeed;
-
+        
+        Velocity = direction * (float)MoveSpeed;
+        
         MoveAndSlide();
     }
 }
