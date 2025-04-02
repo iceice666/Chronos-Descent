d0cdf9403ce04f9794861badf0b88e9871d066c6
Brian Duan <iceice666@outlook.com>
2025-04-02 19:06:34 +0800

    update 4.4 export config

--- Builder/godot_export.sh
+++ Builder/godot_export.sh
@@ -12,6 +12,9 @@ BUILD_TYPE="${2:-debug}"
 EXPORT_FLAG="--export-${BUILD_TYPE}"
 EXPORT_OUTPUT="/project/build/${EXPORT_NAME}_${BUILD_TARGET}_${BUILD_TYPE}.apk"
 
+# Trim the addons
+rm -rf /project/addons
+
 echo "Building ${BUILD_TYPE} version of ${EXPORT_NAME}..."
 
 # Export the Game using Godot
